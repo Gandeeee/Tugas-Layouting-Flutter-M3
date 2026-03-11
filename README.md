484dca3f7315b360138dc0d7dc8c03c56d7b33d0
Gandeeee <gandhisuastika@gmail.com>
2026-03-11 19:41:31 +0700

    Tugas M3 basic Layoutting berdasarkan sumber source code

---
+++ README.md
@@ -0,0 +1,3 @@
+# tugas_m3_flutter_layouting
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/tugas_m3_flutter_layouting/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tugas_m3_flutter_layouting
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
